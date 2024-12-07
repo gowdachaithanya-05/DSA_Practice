28d8da65f55458a698f18933f087547855e65fdc
gowdachaithanya-05 <chaithanyargowda2003@gmail.com>
2024-12-07 23:15:50 +0530

    practice

--- Practice/prime.cpp
+++ Practice/prime.cpp
@@ -1,4 +1,3 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -18,7 +17,6 @@ using namespace std;
  }
  }
 int main() {
-    // Write C++ code here
     int n;
     cin >> n;
     if (prime(n)){

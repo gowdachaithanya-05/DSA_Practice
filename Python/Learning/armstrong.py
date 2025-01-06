21a591767032355c4e8268fbda3c1e7c69bc3b08
gowdachaithanya-05 <chaithanyargowda2003@gmail.com>
2025-01-06 23:25:20 +0530

    armstrong number

---
+++ Python/Learning/armstrong.py
@@ -0,0 +1,28 @@
+# # Breute force method 
+# """step 1: convert to string 
+#    step 2: add pow of length for all the digit
+#    step 3: return n == sum """
+def arm(n):
+    s = str(n)
+    l = len(s)
+    sum = 0
+    for i in s:
+        sum = sum + pow(int(i),l)
+
+    return n==sum
+
+a = arm(371)
+print(a)
+
+"""Mathematical approach without converting to str
+same logic of reverse number"""
+def arm(n):
+    sum = 0
+    temp = n
+    while temp > 0:
+        digit = temp % 10
+        sum = sum + pow(digit, len(str(n)))
+        temp = temp // 10
+    return n == sum 
+
+print(arm(371))
